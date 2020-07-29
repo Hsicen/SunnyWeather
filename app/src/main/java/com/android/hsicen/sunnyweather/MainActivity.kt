db9ca57d68711a5e6a94807df03b91c461b0080c
hsicen <codinghuang@163.com>
2020-07-29 09:42:00 +0800

    创建项目，首次提交

---
+++ app/src/main/java/com/android/hsicen/sunnyweather/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.hsicen.sunnyweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
